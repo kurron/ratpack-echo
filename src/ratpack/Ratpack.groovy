40b7c8e500a2a3539c3eaa9abb4cabe5bb551fc4
Ron Kurr <kurr@kurron.org>
2016-09-04 12:04:09 -0400

    Installed the Ratpack plugin and can now launch from Gradle.

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kurron.example
+
 
 /**
  * Simple web application that serves up the current ip and instance information.
